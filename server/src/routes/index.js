e5459f219f5a494a0fe8802e978627dea015554c
hridaya <hridayamishra225@gmail.com>
2025-02-19 20:53:37 +0545

    new router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,29 +1,28 @@
-import {Router} from 'express'
-import userRouter from './userRoutes.js'
-import journalRouter from './journalRoutes.js'
-import travelGroupRouter from './travelgroupRoutes.js'
-import expenseRouter from './expenseRouter.js'
-import travelRouter from './travelgroupRoutes.js'
-const router = Router()
+import { Router } from "express";
+import userRouter from "./userRoutes.js";
+import journalRouter from "./journalRoutes.js";
+import travelGroupRouter from "./travelgroupRoutes.js";
+import expenseRouter from "./expenseRouter.js";
+import travelRouter from "./travelgroupRoutes.js";
+const router = Router();
 
-// * ROUTES FOR THE USER 
-router.use('/api/v1/user',userRouter)
+// * ROUTES FOR THE USER
+router.use("/api/v1/user", userRouter);
 
 // * ROUTES FOR THE JOURNAL
-router.use('/api/v1/journal',journalRouter)
+router.use("/api/v1/journal", journalRouter);
 
 // * ROUTES FOR THE TRAVEL GROUP
-router.use('/api/v1/group', travelGroupRouter)
+router.use("/api/v1/group", travelGroupRouter);
 
 // * ROUTES FOR THE Expenses
-router.use('/api/v1/expense', expenseRouter)
+router.use("/api/v1/expense", expenseRouter);
 
 // * ROUTES FOR THE Travel
-router.use('/api/v1/travelGroup', travelRouter)
+router.use("/api/v1/travelGroup", travelRouter);
 // * ROUTES FOR THE SETTLEMENT
+router.use("/api/v1/settlement");
 
 // * ROUTES FOR THE NOTIFICATION
 
-
-
-export default router
+export default router;
