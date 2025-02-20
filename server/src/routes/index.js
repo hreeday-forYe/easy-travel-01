e28ff8d11945a1a04fb4b51640d8a1688546a02d
Naresh123se <naresh132na@gmail.com>
2025-02-20 13:31:21 +0545

    update

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -21,7 +21,7 @@ router.use("/api/v1/expense", expenseRouter);
 // * ROUTES FOR THE Travel
 router.use("/api/v1/travelGroup", travelRouter);
 // * ROUTES FOR THE SETTLEMENT
-router.use("/api/v1/settlement");
+// router.use("/api/v1/settlement");
 
 // * ROUTES FOR THE NOTIFICATION
 
